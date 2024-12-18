ce0605c245a34279ed49230631e4a4800076e21b
Nicolás Romero <niromero@frba.utn.edu.ar>
2024-12-18 00:35:20 -0300

    Assignments deleted

--- assignments/mail/run.sh
+++
@@ -1,4 +0,0 @@
-mvn install 
-mvn compile
-
-jshell --class-path target/app-1.0.0.jar init.jsh
--- assignments/pokemon/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { PokemonInterface } from "./services/pokemon/interface";
-
-const main = () => {
-	new PokemonInterface().start();
-};
-
-main();
